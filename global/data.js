ac9d0c2707e2125a9f4522674b605e35b51eeeb4
DSS T Level for TMC <dctmdec2005@gmail.com>
2023-02-19 18:22:04 +0000

    a

--- global/data.js
+++ global/data.js
@@ -8,118 +8,25 @@ document.getElementById("footer").innerHTML =
         <li class="list-inline-item"><a href="https://gmganddart.github.io/README.md">About</a></li>
         <li class="list-inline-item"><a href="#">Top of Page</a></li>
     </ul>
-    <p class="copyright">© DM at TMC</p>
+    <p class="copyright">Website</p>
   </footer>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>function autoRefresh() {window.location = window.location.href;}setInterval('autoRefresh()', 600000);</script>
 `
 
 document.getElementById("navigbar").innerHTML = 
 `
 <div class="nav_ul">
   <ul id="nav_ul">
-  <a href="https://dsstlevel.github.io/index.html"><i class="fa fa-home" style="font-size:20px;color:white"></i></a>
+  <a href="https://gmganddart.github.io/index.html"><i class="fa fa-home" style="font-size:20px;color:white"></i></a>
   <li class="dropdown">
-  <a><i class="fa-solid fa-bars"> </i><i class="fa-solid fa-a"></i></a>
+  <a>Main Webs</a>
   <ul class="dropdown-content">
     <li class="dropdown-sub">
-      <a>Business Context</a>
+      <a>Personal</a>
       <ul class="dropdown-sub-list">
-      <li><a href="https://dsstlevel.github.io/Web/html/BusinessContext.html">Context</a></li>
-      <li><a href="https://dsstlevel.github.io/Web/html/Bus-Terminology.html">Terminology</a></li>
+      <li><a href="https://gmganddart.github.io/Web/Gmg/Main.html">Gmg</a></li>
       </ul></li>
-    <li class="dropdown-sub">  
-      <a>Culture</a>
-      <ul class="dropdown-sub-list">
-      </ul></li>
-    <li class="dropdown-sub">
-      <a>Digital Environments</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://dsstlevel.github.io/Web/html/Soft-Hardware.html">Software/Hardware</a></li>
-        <li><a href="https://dsstlevel.github.io/Web/html/Networking.html">Networking</a></li>
-        </ul></li>
-    <li class="dropdown-sub">
-      <a>Diversity and Inclusion</a>
-      <ul class="dropdown-sub-list">
-        <li></li>
-          <a href="https://dsstlevel.github.io/Web/html/EqualityAct.html">Equality Act</a>
-        </ul></li>
-    <li class="dropdown-sub">
-      <a>Learning</a>
-      <ul class="dropdown-sub-list">
-      </ul></li>
-    <li class="dropdown-sub">
-      <a>Planning</a>
-      <ul class="dropdown-sub-list">
-      </ul></li>
-    </li>
-  </ul>
-  </li>
-  <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i> <i class="fa-solid fa-b"></i></a>
-  <ul class="dropdown-content">
-    <li class="dropdown-sub">
-      <a>Testing</a>
-      <ul class="dropdown-sub-list">
-      <li><a href="https://dsstlevel.github.io/Web/html/RouteCauseAnalysis.html">Route Cause Analysis</a></li>
-      </ul></li>
-    <li class="dropdown-sub">  
-      <a>Tools</a>
-      <ul class="dropdown-sub-list">
-      </ul></li>
-    <li class="dropdown-sub">
-      <a>Legislation</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://dsstlevel.github.io/Web/html/Laws-Regs.html">Laws&Regs</a></li>
-        </ul></li>
-    <li class="dropdown-sub">
-      <a>Security</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://dsstlevel.github.io/Web/html/Threats.html">Threats</a></li>
-        </ul></li>
-    <li class="dropdown-sub">
-      <a>Data</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://dsstlevel.github.io/Web/html/Data.html">Data Info</a></li>
-        </ul></li>
-    <li class="dropdown-sub">
-      <a>Data Analysis</a>
-      <ul class="dropdown-sub-list">
-      </ul></li>
-    </li>
-  </ul>
-  </li>
-  <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i> <i class="fa-solid fa-newspaper"></i></a>
-  <ul class="dropdown-content">
-    <li class="dropdown-sub">
-      <a href="https://www.netacad.com/portal/learning" target="_blank">Cisco</a>
-      <a href="https://dsstlevel.github.io/Web/html/codingScripts">Scripts (Quizzes)</a>
-      <li class="dropdown-sub">
-      <a>NCFE</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://ncfe.org.uk" target="_blank">NCFE Website</a></a>
-        <li><a href="https://www.qualhub.co.uk/qualification-search/qualification-detail/t-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034?login=false&originalUrl=https%3A%2F%2Fwww.qualhub.co.uk.mcas.ms%2Fqualification-search%2Fqualification-detail%2Ft-level-technical-qualification-in-digital-support-services-level-3-delivered-by-5034%3FMcasTsid%3D20892&McasCSRF=2a04a17a9a924bcc526b4bd772189cf149d6804c98bb8c2c08d559e394f185f9" target="_blank">DSS Past Papers</a></li>
-        </ul></li>
-      <a href="https://www.amazon.co.uk/Digital-Level-Support-Services-Business/dp/1398346799?source=ps-sl-shoppingads-lpcontext&ref_=fplfs&psc=1&smid=A3P5ROKL5A1OLE">Revision Book</a>
-      <li class="dropdown-sub">
-      <a>TMC</a>
-      <ul class="dropdown-sub-list">
-        <li><a href="https://studenthub.tmc.ac.uk">Student Hub</a></li>
-        <li><a href="https://proportal.tmc.ac.uk">ProPortal</a></li>
-        <li><a href="https://grofar.com/">GroFar</a></li>
-        </ul></li>
-      </li>
-  </ul>
-  </li>
-  <li class="dropdown">
-  <a><i class="fa-solid fa-bars"></i>Info</a>
-  <ul class="dropdown-content">
-    <li class="dropdown-sub">
-      <a href="https://dsstlevel.github.io/Web/html/impdates.html">Dates/Events</a>
-      <a href="https://dsstlevel.github.io/Web/html/year1.html">Examination Details</a>
-    </li>
   </ul>
   </li>
 </div>
