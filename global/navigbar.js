2ad35c10e8db6d441c8cc28498416e574316afeb
gmgwhDM2005 <dctmdec2005@gmail.com>
2023-02-20 15:47:31 +0000

    a

--- global/navigbar.js
+++ global/navigbar.js
@@ -1,21 +1,4 @@
-document.getElementById("footer").innerHTML = 
-`
-<style></style>
-<div class="footer-basic">
-  <footer>
-    <ul class="list-inline">
-        <li class="list-inline-item"><a href="https://gmganddart.github.io/index.html">Home</a></li>
-        <li class="list-inline-item"><a href="https://gmganddart.github.io/README.md">About</a></li>
-        <li class="list-inline-item"><a href="#">Top of Page</a></li>
-    </ul>
-    <p class="copyright">Website</p>
-  </footer>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-`
-
-document.getElementById("navigbar").innerHTML = 
-`
+document.getElementById("navigbar").innerHTML = `
 <div class="nav_ul">
   <ul id="nav_ul">
   <a href="https://gmganddart.github.io/index.html"><i class="fa fa-home" style="font-size:20px;color:black"></i></a>
@@ -32,4 +15,4 @@ document.getElementById("navigbar").innerHTML =
   </li>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-`
+`;
