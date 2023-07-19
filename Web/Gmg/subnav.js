c09922ec70d008d2fa136a523d2bfffaa80ef4a7
gmgwhDM2005 <107148267+gmgwhDM2005@users.noreply.github.com>
2023-07-19 13:40:43 +0100

    a

--- Web/Gmg/subnav.js
+++ Web/Gmg/subnav.js
@@ -5,18 +5,18 @@ document.getElementById("subnavigbar").innerHTML = `
 <nav class="subnavbar">
     <ul class="subnavbar-list">
         <li class="subnavbar-item">
-            <a href="#" class="subnavbar-link">Databases</a>
+            <a href="#" class="subnavbar-link">About Me</a>
             <ul class="subdropdown-menu">
-                <li><a href="https://gmganddart.github.io/Web/databases/example.json" class="subdropdown-link">Example.json</a></li>
+                <li><a href="https://gmganddart.github.io/Web/Gmg/Buses/buses" class="subdropdown-link">Bus Photography</a></li>
             </ul>
         </li>
         <li class="subnavbar-item">
-            <a href="https://gmganddart.github.io/Web/Gmg/Main.html" class="subnavbar-link"><i class="fa fa-home"></i></a>
+            <a href="https://gmganddart.github.io/Web/Gmg/Main" class="subnavbar-link"><i class="fa fa-home"></i></a>
         </li>
         <li class="subnavbar-item">
             <a href="#" class="subnavbar-link">Databases</a>
             <ul class="subdropdown-menu">
-                <li><a href="https://gmganddart.github.io/Web/databases/example.json" class="subdropdown-link">Example.json</a></li>
+                <li><a href="https://gmganddart.github.io/Web/Gmg/buses.json" class="subdropdown-link">Buses</a></li>
             </ul>
         </li>
     </ul>
